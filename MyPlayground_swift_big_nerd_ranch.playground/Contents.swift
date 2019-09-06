6f1f158f58b86767ce2e3cc0acd3b5465460e5ab
Emmanuel López Guerrero <manelpz@icloud.com>
2019-09-06 00:49:22 -0500

    Adding switch, tuples and if-case

--- MyPlayground_swift_big_nerd_ranch.playground/Contents.swift
+++ MyPlayground_swift_big_nerd_ranch.playground/Contents.swift
@@ -21,3 +21,42 @@ if !hasPostOffice{
 //ternary
 
 message = population < 1000 ? "\(population) is a small": "\(population) is big city"
+
+
+//switch
+
+
+var statusCode: Int = 24
+
+var errorString: String = "The request failed"
+
+switch statusCode {
+case 100, 101:
+    errorString += "information \(statusCode)"
+case 204:
+    errorString += "succefull but not content \(statusCode)"
+case 400 ... 407:
+    errorString += "Client error \(statusCode)"
+case let unknowncode where (unknowncode >= 200 && unknowncode < 300) || unknowncode > 505:
+    errorString += "\(unknowncode) is not know error"
+default:
+    errorString += "something"
+}
+
+
+//tuplas
+
+let error = (code: statusCode, error:errorString)
+
+error.code
+error.error
+
+//if-case
+
+let age = 24
+if case 18...25 = age, age >= 21
+{
+    print("drinking age")
+}
+
+
