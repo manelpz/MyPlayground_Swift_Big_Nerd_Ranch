da3ad3550a17da8b80e5c37f136e81e1363f5419
Emmanuel López Guerrero <manelpz@icloud.com>
2019-11-09 23:43:10 -0600

    Closures Capture Values

--- MyPlayground_swift_big_nerd_ranch.playground/Contents.swift
+++ MyPlayground_swift_big_nerd_ranch.playground/Contents.swift
@@ -524,7 +524,7 @@ if let newTownPlan = makeTownGrand(budget: 100, condition: evaluatedBudget){
 }
 
 print("Knowhere has \(stoplights) stoplights.")
-
+/*
 
 func makeTownGrand(budget: Int, condition: (Int) -> Bool) -> ((Int,Int) -> Int)? {
     if condition(budget){
@@ -535,7 +535,7 @@ func makeTownGrand(budget: Int, condition: (Int) -> Bool) -> ((Int,Int) -> Int)?
     }else{
         return nil
     }
-}
+}*/
 
 
 
