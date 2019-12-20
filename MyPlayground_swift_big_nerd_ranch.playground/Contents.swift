d79fa89db6234aec1625e668c72f5255da516d1f
Emmanuel López Guerrero <manelpz@icloud.com>
2019-12-19 23:29:25 -0600

    bigProjections

--- MyPlayground_swift_big_nerd_ranch.playground/Contents.swift
+++ MyPlayground_swift_big_nerd_ranch.playground/Contents.swift
@@ -579,27 +579,6 @@ anothergrowBy500()
  (population: Int)-> Int in
  return population*2
  }
- projectedPopulations
 
 
 
-let bigProjections = projectedPopulations.filter {
-    (projection: Int) -> Bool in
-    return projection > 400
-}
-bigProjections
-
-let precinctPopulations = [1244,2021,2157]
-let projectedPopulations = precinctPopulations.map{
-    (population: Int)-> Int in
-    return population*2
-}
-projectedPopulations
-
-
-
-let bigProjections = projectedPopulations.filter {
-    (projection: Int) -> Bool in
-    return projection > 400
-}
-bigProjections
