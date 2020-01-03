e93fd6c58808fa2ef37be3bc475ac10fbaf740bf
Emmanuel López Guerrero <manelpz@icloud.com>
2020-01-02 23:55:05 -0600

    functional programming

--- MyPlayground_swift_big_nerd_ranch.playground/Contents.swift
+++ MyPlayground_swift_big_nerd_ranch.playground/Contents.swift
@@ -581,25 +581,3 @@ anothergrowBy500()
  }
 
 
-
-let precinctPopulations = [1244,2021,2157]
-let projectedPopulations = precinctPopulations.map{
-    (population: Int)-> Int in
-    return population*2
-}
-
-let precinctPopulations = [1244,2021,2157]
-let projectedPopulations = precinctPopulations.map{
-    (population: Int)-> Int in
-    return population*2
-}
-
-
-
-let precinctPopulations = [1244,2021,2157]
-let projectedPopulations = precinctPopulations.map{
-    (population: Int)-> Int in
-    return population*2
-}
-
-
