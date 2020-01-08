afcf30854b9bb418e6054384e3ebf338aa2332e2
Emmanuel López Guerrero <manelpz@icloud.com>
2020-01-07 23:21:01 -0600

    functional programming

--- MyPlayground_swift_big_nerd_ranch.playground/Contents.swift
+++ MyPlayground_swift_big_nerd_ranch.playground/Contents.swift
@@ -580,4 +580,9 @@ anothergrowBy500()
  return population*2
  }
 
+let precinctPopulations = [1244,2021,2157]
+let projectedPopulations = precinctPopulations.map{
+    (population: Int)-> Int in
+    return population*2
+}
 
